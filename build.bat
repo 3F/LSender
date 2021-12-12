43a7d110f08e5be54409afb5ff0add9f1bff1200
Denis Kuzmin (github/3F) <x-3F@outlook.com>
2021-12-12 18:24:04 +0300

    Updated TFMs, added common.props + redefine.props
    updated vsSolutionBuildEvent 1.14.1.1 + tools (hMSBuild 2.3, GetNuTool 1.8)

--- build.bat
+++ build.bat
@@ -1,25 +1,15 @@
 @echo off
 
 set cim=packages\vsSolutionBuildEvent\cim.cmd
-set _gnt=tools/gnt
 
-set reltype=%~1
-if "%reltype%"=="" (
-    set "reltype=DCI"
-)
+set reltype=%1
+if not defined reltype set reltype=DCI
 
-:: Activate vsSBE
-call %_gnt% /p:wpath="%cd%" /p:ngconfig=".gnt/packages.config" /nologo /v:m /m:4 || goto err
+call tools/gnt /p:wpath="%cd%" /p:ngconfig="tools/packages.config" /nologo /v:m /m:7 || goto err
+call %cim% /v:m /m:7 /p:Configuration="%reltype%_SDK15" || goto err
 
-:: Build
-call %cim% "LSender.sln" /v:m /m:4 /p:Configuration="%reltype%_SDK15" || goto err
-
-goto ok
+exit /B 0
 
 :err
-
 echo. Build failed. 1>&2
 exit /B 1
-
-:ok
-exit /B 0
